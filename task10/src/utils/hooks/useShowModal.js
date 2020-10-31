8d0a2a57fa751149d81180cde485e692fda00c45
Ricardo Quinonez <vir_qup_91@hotmail.com>
2020-10-31 11:43:35 -0600

    Remove original scafolding

--- task10/src/utils/hooks/useShowModal.js
+++
@@ -1,8 +0,0 @@
-import {useState} from 'react';
-
-export default function useShowModal() {
-  const [showModal, setShowModal] = useState(false);
-  const toggleModal = () => setShowModal(!showModal);
-
-  return [showModal, toggleModal];
-}
--- task10/src/utils/logo/logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './logo.scss';
-
-function Logo() {
-  return (<h1 className="logo"><span>Netflix</span>roullete</h1>);
-}
-
-export default Logo;
